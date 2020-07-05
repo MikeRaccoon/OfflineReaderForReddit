ebe80667f456b73e9eeacec91de5470191ff862a
Mike <cyberenot@gmail.com>
2020-07-05 18:39:40 +0300

    spinner reposition, safeAreaLayoutGuide fix

--- OfflineReaderForReddit/SpinnerViewController.swift
+++ OfflineReaderForReddit/SpinnerViewController.swift
@@ -13,13 +13,12 @@ class SpinnerViewController: UIViewController {
 
     override func loadView() {
         view = UIView()
-        view.backgroundColor = UIColor(white: 0, alpha: 0.7)
 
         spinner.translatesAutoresizingMaskIntoConstraints = false
         spinner.startAnimating()
         view.addSubview(spinner)
 
         spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        spinner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
     }
 }
